ff72846c536af0ae39029cbf18725c252832024c
Yinli <yinli_free@pku.edu.cn>
2019-04-22 20:10:50 +0800

    Delete test.py

--- tuniu_jingdian/test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-
-conn = pymysql.connect(host='114.116.43.151', user='root', passwd='sspku02!',db='tuniu')
-cur = conn.cursor()
-cur.execute('insert into area(id) value(456)')
-#print('test' in list(map(lambda x:x[0],cur.fetchall())))
-#cur.close()
-conn.commit()
-#conn.close()
